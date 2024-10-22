08c266611938f5681334372b0c6420da8002df7d
cabobalot <Casey8cockrell@gmail.com>
2024-10-21 20:49:30 -0600

    betterTestBench

--- quartusProject/tb_ALU.v
+++ quartusProject/tb_ALU.v
@@ -1,49 +1,123 @@
+
 //testbench for an ALU
 
 module tb_alu;
 
-	// these need to be setup for our ALU
-	parameter ALUADD             =  3'd2;
-	parameter ALUSUB             =  3'd1;
-	parameter ALUOR              =  3'd1;
-	parameter ALUAND             =  3'd0;
-	parameter ALUXOR             =  3'd4;
-	parameter ALUSHIFT           =  3'd5;
-	parameter ALUSHIFTSIGNEXTEND =  3'd6;
-	parameter ALUMUL             =  3'd7;
+	parameter AND = 3'b000;
+	parameter OR  = 3'b001;
+	parameter XOR = 3'b010;
+	parameter ADD = 3'b011;
+	parameter SUB = 3'b100;
+	parameter LSHIFT = 3'b101;
+	parameter RSHIFT = 3'b110;
 
-
-	// these are all too small rn
-	reg [7:0] src1, src2;
+	reg [15:0] src1, src2;
 	reg [2:0] controlCode;
-	wire [7:0] result;
+	wire [15:0] result;
+	
+	wire carry, low, flag, zero, negative;
 
-	alu DUT (src1, src2, controlCode, result);
+
+	alu DUT (controlCode, src1, src2, result, carry, low, flag, zero, negative);
 
 	initial begin
 		$display("BeginALU test");
 
+		
+
+		// And test
+		controlCode = AND;
+
+		src1 = 65535;
+		src2 = 65535;
+		#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
+
+		src1 = 65535;
+		src2 = 0;
+		#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
+
+		src1 = 0;
+		src2 = 65535;
+		#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
+
+		for(src1 = 0; src1 < 65490; src1 = src1 + 19) begin
+			for(src2 = 0; src2 < 65490; src2 = src2 + 37) begin
+				#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
+			end
+		end
+
+		$display("And tests done");
+
+
+		// Or test
+		controlCode = OR;
+
+		src1 = 65535;
+		src2 = 65535;
+		#1 if (result != (src1 | src2)) $display("Or error ", src1, " | ", src2, ", expected: ",  src1 | src2, ", got: ", result);
+
+		src1 = 65535;
+		src2 = 0;
+		#1 if (result != (src1 | src2)) $display("Or error ", src1, " | ", src2, ", expected: ",  src1 | src2, ", got: ", result);
+
+		src1 = 0;
+		src2 = 65535;
+		#1 if (result != (src1 | src2)) $display("Or error ", src1, " | ", src2, ", expected: ",  src1 | src2, ", got: ", result);
+
+		for(src1 = 0; src1 < 65490; src1 = src1 + 19) begin
+			for(src2 = 0; src2 < 65490; src2 = src2 + 37) begin
+				#1 if (result != (src1 | src2)) $display("Or error ", src1, " | ", src2, ", expected: ",  src1 | src2, ", got: ", result);
+			end
+		end
+
+		$display("Or tests done");
+
+
+		// XOR test
+		controlCode = XOR;
+
+		src1 = 65535;
+		src2 = 65535;
+		#1 if (result != (src1 ^ src2)) $display("Xor error ", src1, " ^ ", src2, ", expected: ",  src1 ^ src2, ", got: ", result);
+
+		src1 = 65535;
+		src2 = 0;
+		#1 if (result != (src1 ^ src2)) $display("Xor error ", src1, " ^ ", src2, ", expected: ",  src1 ^ src2, ", got: ", result);
+
+		src1 = 0;
+		src2 = 65535;
+		#1 if (result != (src1 ^ src2)) $display("Xor error ", src1, " ^ ", src2, ", expected: ",  src1 ^ src2, ", got: ", result);
+
+		for(src1 = 0; src1 < 65490; src1 = src1 + 19) begin
+			for(src2 = 0; src2 < 65490; src2 = src2 + 37) begin
+				#1 if (result != (src1 ^ src2)) $display("Xor error ", src1, " ^ ", src2, ", expected: ",  src1 ^ src2, ", got: ", result);
+			end
+		end
+
+		$display("Xor tests done");
+
+
 		// addition tests
 		src1 = 0;
 		src2 = 0;
-		controlCode = ALUADD;
+		controlCode = ADD;
 
 		#1 if (result != (src1 + src2)) $display("Addition error ", src1, " + ", src2, ", expected: ",  src1 + src2, ", got: ", result);
 		
-		src1 = 255;
-		src2 = 255;
+		src1 = 65535;
+		src2 = 65535;
 		#1 if (result != (src1 + src2)) $display("Addition error ", src1, " + ", src2, ", expected: ",  src1 + src2, ", got: ", result);
 
-		src1 = 255;
+		src1 = 65535;
 		src2 = 1;
 		#1 if (result != (src1 + src2)) $display("Addition error ", src1, " + ", src2, ", expected: ",  src1 + src2, ", got: ", result);
 		
 		src1 = 1;
-		src2 = 255;
+		src2 = 65535;
 		#1 if (result != (src1 + src2)) $display("Addition error ", src1, " + ", src2, ", expected: ",  src1 + src2, ", got: ", result);
 
-		for(src1 = 0; src1 < 240; src1 = src1 + 7) begin
-			for(src2 = 0; src2 < 240; src2 = src2 + 13) begin
+		for(src1 = 0; src1 < 65490; src1 = src1 + 19) begin
+			for(src2 = 0; src2 < 65490; src2 = src2 + 37) begin
 				#1 if (result != (src1 + src2)) $display("Addition error ", src1, " + ", src2, ", expected: ",  src1 + src2, ", got: ", result);
 			end
 		end
@@ -51,28 +125,172 @@ module tb_alu;
 		$display("Addition tests done");
 
 
-		controlCode = ALUAND;
-
-		src1 = 255;
-		src2 = 255;
-		#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
-
-		src1 = 255;
-		src2 = 0;
-		#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
-
+		// subtratct tests
 		src1 = 0;
-		src2 = 255;
-		#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
+		src2 = 0;
+		controlCode = SUB;
 
-		for(src1 = 0; src1 < 240; src1 = src1 + 7) begin
-			for(src2 = 0; src2 < 240; src2 = src2 + 13) begin
-				#1 if (result != (src1 & src2)) $display("And error ", src1, " & ", src2, ", expected: ",  src1 & src2, ", got: ", result);
+		#1 if (result != (src1 - src2)) $display("Subtract error ", src1, " - ", src2, ", expected: ",  src1 - src2, ", got: ", result);
+		
+		src1 = 65535;
+		src2 = 65535;
+		#1 if (result != (src1 - src2)) $display("Subtract error ", src1, " - ", src2, ", expected: ",  src1 - src2, ", got: ", result);
+
+		src1 = 65535;
+		src2 = 1;
+		#1 if (result != (src1 - src2)) $display("Subtract error ", src1, " - ", src2, ", expected: ",  src1 - src2, ", got: ", result);
+		
+		src1 = 1;
+		src2 = 65535;
+		#1 if (result != (src1 - src2)) $display("Subtract error ", src1, " - ", src2, ", expected: ",  src1 - src2, ", got: ", result);
+
+		for(src1 = 0; src1 < 65490; src1 = src1 + 19) begin
+			for(src2 = 0; src2 < 65490; src2 = src2 + 37) begin
+				#1 if (result != (src1 - src2)) $display("Subtract error ", src1, " - ", src2, ", expected: ",  src1 - src2, ", got: ", result);
+			end
+		end
+		
+		$display("Subtract tests done");
+
+		
+
+		//R shift tests
+		
+		controlCode = RSHIFT;
+		src1 = 0;
+		src2 = 0;
+		
+		#1 if (result != (src1 >> src2[3:0])) $display("R shift error ", src1, " >> ", src2, ", expected: ",  src1 >> src2[3:0], ", got: ", result);
+
+		src1 = 65535;
+		src2 = 16;
+		
+		#1 if (result != (src1 >> src2[3:0])) $display("R shift error ", src1, " >> ", src2, ", expected: ",  src1 >> src2[3:0], ", got: ", result);
+
+		src1 = 65535;
+		src2 = 0;
+		
+		#1 if (result != (src1 >> src2[3:0])) $display("R shift error ", src1, " >> ", src2, ", expected: ",  src1 >> src2[3:0], ", got: ", result);
+
+		for(src1 = 0; src1 < 65490; src1 = src1 + 37) begin
+			for(src2 = 0; src2 < 16; src2 = src2 + 1) begin
+				#1 if (result != (src1 >> src2[3:0])) $display("R shift error ", src1, " >> ", src2, ", expected: ",  src1 >> src2[3:0], ", got: ", result);
 			end
 		end
 
-		$display("And tests done");
+		$display("RShift tests done");
 
+
+		//LShift tests
+		controlCode = LSHIFT;
+		src1 = 0;
+		src2 = 0;
+		
+		#1 if (result != (src1 << src2[3:0])) $display("L shift error ", src1, " << ", src2, ", expected: ",  src1 << src2[3:0], ", got: ", result);
+
+		src1 = 65535;
+		src2 = 16;
+		
+		#1 if (result != (src1 << src2[3:0])) $display("L shift error ", src1, " << ", src2, ", expected: ",  src1 << src2[3:0], ", got: ", result);
+
+		src1 = 65535;
+		src2 = 0;
+		
+		#1 if (result != (src1 << src2[3:0])) $display("L shift error ", src1, " << ", src2, ", expected: ",  src1 << src2[3:0], ", got: ", result);
+
+		for(src1 = 0; src1 < 65490; src1 = src1 + 37) begin
+			for(src2 = 0; src2 < 16; src2 = src2 + 1) begin
+				#1 if (result != (src1 << src2[3:0])) $display("L shift error ", src1, " << ", src2, ", expected: ",  src1 << src2[3:0], ", got: ", result);
+			end
+		end
+
+		$display("L Shift tests done");
+
+
+		//Test flags
+
+
+		controlCode = ADD;
+
+		//carry
+		src1 = 2;
+		src2 = 3;
+		#1 if (carry) $display("Carry error ", src1, " + ", src2, ", expected: ",  0, ", got: ", carry);
+
+		src1 = 65535;
+		src2 = 65535;
+		#1 if (!carry) $display("Carry error ", src1, " + ", src2, ", expected: ",  1, ", got: ", carry);
+
+		src1 = 60000;
+		src2 = 34567;
+		#1 if (!carry) $display("Carry error ", src1, " + ", src2, ", expected: ",  1, ", got: ", carry);
+
+		//low
+		src1 = 1;
+		src2 = 0;
+		#1 if (low) $display("Low error ", src1, " + ", src2, ", expected: ",  0, ", got: ", low);
+
+		src1 = 0;
+		src2 = 1;
+		#1 if (!low) $display("Low error ", src1, " + ", src2, ", expected: ",  1, ", got: ", low);
+
+
+		//flag / signed overflow
+		src1 = $signed(5);
+		src2 = $signed(10);
+		#1 if (flag) $display("Flag error ", src1, " + ", src2, ", expected: ",  0, ", got: ", flag);
+
+		src1 = $signed(-8);
+		src2 = $signed(-20);
+		#1 if (flag) $display("Flag error ", src1, " + ", src2, ", expected: ",  0, ", got: ", flag);
+
+		src1 = $signed(32765);
+		src2 = $signed(100);
+		#1 if (!flag) $display("Flag error ", src1, " + ", src2, ", expected: ",  1, ", got: ", flag);
+
+		src1 = $signed(-32765);
+		src2 = $signed(-100);
+		#1 if (!flag) $display("Flag error ", src1, " + ", src2, ", expected: ",  1, ", got: ", flag);
+
+
+		//zero
+		src1 = $signed(-5);
+		src2 = $signed(5);
+		#1 if (!zero) $display("zero error ", src1, " + ", src2, ", expected: ",  1, ", got: ", zero);
+
+		src1 = $signed(5);
+		src2 = $signed(5);
+		#1 if (zero) $display("zero error ", src1, " + ", src2, ", expected: ",  0, ", got: ", zero);
+		
+		controlCode = AND;
+		src1 = $signed(255);
+		src2 = $signed(255);
+		#1 if (zero) $display("zero error ", src1, " & ", src2, ", expected: ",  0, ", got: ", zero);
+
+		src1 = $signed(1);
+		src2 = $signed(2);
+		#1 if (!zero) $display("zero error ", src1, " & ", src2, ", expected: ",  1, ", got: ", zero);
+		controlCode = ADD;
+
+		//negative
+		src1 = $signed(1);
+		src2 = $signed(0);
+		#1 if (negative) $display("negative error ", src1, " + ", src2, ", expected: ",  0, ", got: ", negative);
+
+		src1 = $signed(0);
+		src2 = $signed(1);
+		#1 if (!negative) $display("negative error ", src1, " + ", src2, ", expected: ",  1, ", got: ", negative);
+
+		src1 = $signed(5);
+		src2 = $signed(-5);
+		#1 if (negative) $display("negative error ", src1, " + ", src2, ", expected: ",  0, ", got: ", negative);
+
+		src1 = $signed(-100);
+		src2 = $signed(-50);
+		#1 if (!negative) $display("negative error ", src1, " + ", src2, ", expected: ",  1, ", got: ", negative);
+
+
+		$display("Flags tests done");
 
 
 		#5 $display("All tests finished.");
@@ -82,4 +300,6 @@ module tb_alu;
 
 
 endmodule
+
+
 
