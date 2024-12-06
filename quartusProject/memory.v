1d07284e9516df06e394acfc6fffa9c72212dd45
cabobalot <Casey8cockrell@gmail.com>
2024-12-05 22:40:05 -0700

    compile I think?

--- quartusProject/memory.v
+++ quartusProject/memory.v
@@ -7,7 +7,7 @@ module memory
 
     reg [15:0] ram[2**16-1:0];
     initial begin
-        $readmemh("C:/Users/dhhoy/3710-CPU/assembler/game.dat", ram);
+        $readmemh("/home/casey/Documents/School/ECE3710/3710-CPU/assembler/game.dat", ram);
     end
 
     // Port A
